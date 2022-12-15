902226e6403a4c9bff270fadb16a54f7b355caab
gucci1909 <umangarora0134@gmail.com>
2022-12-16 02:35:43 +0530

    operators-in-java

--- tax_calculator.java
+++ tax_calculator.java
@@ -7,22 +7,21 @@ public class tax_calculator {
         float income = s1.nextFloat(); // LPA
         if(income<=2.5f){
             tax += 0;
-            System.out.println("Your tax amount is :  "+ tax );
+
         }
         else if(income>2.5f && income <=5f){
             tax += 0.05f*(income - 2.5f);
-            System.out.println("Your tax amount is :  "+ tax );
         }
         else if(income>5f && income <=7.5f){
             tax += 0.05f*(5.0f-2.5f);
             tax += 0.1f*(income-5.0f);
-            System.out.println("Your tax amount is :  "+ tax );
+
         }
         else if(income>7.5f && income<=10.0f){
             tax += 0.05f*(5.0f-2.5f);
             tax += 0.1f*(income-5.0f);
             tax += 0.15f*(income-7.5f);
-            System.out.println("Your tax amount is :  "+ tax );
+
 
         }
         else if(income>10.0f && income<=12.5f){
@@ -30,7 +29,7 @@ public class tax_calculator {
             tax += 0.1f*(income-5.0f);
             tax += 0.15f*(income-7.5f);
             tax += 0.2f*(income-10f);
-            System.out.println("Your tax amount is :  "+ tax );
+
         }
         else if(income>12.5f && income<=15.0f){
             tax += 0.05f*(5.0f-2.5f);
@@ -38,7 +37,7 @@ public class tax_calculator {
             tax += 0.15f*(income-7.5f);
             tax += 0.2f*(income-10f);
             tax += 0.25f*(income-12.5f);
-            System.out.println("Your tax amount is :  "+ tax );
+
         }
         else{
             tax += 0.05f*(5.0f-2.5f);
@@ -47,8 +46,10 @@ public class tax_calculator {
             tax += 0.2f*(income-10f);
             tax += 0.25f*(income-12.5f);
             tax += 0.3f*(income-15.0f);
-            System.out.println("Your tax amount is :  "+ tax );
+
         }
+
+        System.out.println("Your tax amount is :  "+ tax );
 
     }
 
