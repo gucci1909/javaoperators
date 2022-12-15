2baebe01169f42d5d2e1d79687e3647c772e38a8
gucci1909 <umangarora0134@gmail.com>
2022-12-16 03:41:41 +0530

    operators-in-java

---
+++ for_loop.java
@@ -0,0 +1,9 @@
+public class for_loop {
+    public static void main(String[] args) {
+        for(int i =0 ; i<100;i++){
+            if(i%2==0){
+                System.out.println("All even Numbers from 1 to 100 are : "+ i);
+            }
+        }
+    }
+}
---
+++ star.java
@@ -0,0 +1,11 @@
+public class star {
+    public static void main(String[] args) {
+        int star = 4;
+        for(int i=star;i>0;i--){
+            for(int j =0;j<i;j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
