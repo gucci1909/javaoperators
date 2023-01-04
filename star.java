2df9bc506c0d9a839b3aead62803cf0eefb99fb3
gucci1909 <umangarora0134@gmail.com>
2023-01-04 15:54:08 +0530

    operators-in-java

--- star.java
+++ star.java
@@ -1,5 +1,11 @@
 public class star {
     public static void main(String[] args) {
+
+//        * * * *
+//        * * *
+//        * *
+//        *
+
         int star = 4;
         for(int i=star;i>0;i--){
             for(int j =0;j<i;j++){
@@ -7,5 +13,15 @@ public class star {
             }
             System.out.print("\n");
         }
+
+        // Table of 5
+
+        int n = 5;
+        for(int i =1;i<n;i++){
+            System.out.printf("%d * %d = %d\n",n,i,n*i);
+
+        }
+
+
     }
 }
