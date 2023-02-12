93a161701b6370ec8719c099e3bfbb1798c6ae6c
gucci1909 <umangarora0134@gmail.com>
2023-02-12 21:45:32 +0530

    java file added

---
+++ Hello.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Hello
+{
+    public static void main(String a[]){
+        System.out.println("Hello world!");
+        System.out.println("hello 134");
+        Scanner s1 = new Scanner(System.in);
+        // System.out.println();
+        Scanner s2 = new Scanner(System.in);
+        float f3 = 1345.86f;
+        double f1 = 135.46d;
+        char c = 'a';
+        String abc = "umang";
+        System.out.println(c);
+        System.out.println(abc);
+        System.out.println(c);
+        System.out.println(f1);
+        System.out.println(f3);
+        float income = s1.nextFloat();
+        System.out.println(income);
+        boolean b = true;
+        System.out.println(b);
+        float income1 = s2.nextFloat();
+        System.out.println(income1);
+    }
+}
