f51112895a7b2fccc1072f6d03195129bd8e6b7e
gucci1909 <umangarora0134@gmail.com>
2023-02-14 21:01:16 +0530

    learning java

--- index.java
+++ index.java
@@ -5,6 +5,16 @@ public class index{
         // char num = 'a';
         float num1 = 5.6f;
         int a = (int)num1;
+        boolean value;
+        if(a==num1){
+            value = true;
+            System.out.println("Yes");
+        }
+        else{
+            value = false;
+            System.out.println("No");
+        }
+        System.out.println(value);
         System.out.println(a);
         
 
